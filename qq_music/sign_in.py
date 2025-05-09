73fc332bd750d4d7c5c82440fc5cb82dda86c1b7
Chris <vipfwz@gmail.com>
2025-05-09 09:40:13 +0000

    feat: daily sign in

---
+++ qq_music/sign_in.py
@@ -0,0 +1,59 @@
+import requests
+import os
+import json
+
+import time
+from .zzc_sign import zzc_sign
+from util.bark import send_bark_notification
+
+def current_millis_str() -> str:
+    ms = int(time.time() * 1000)
+    return str(ms).zfill(13)
+
+MUSIC_KEY = os.getenv("MUSIC_KEY", "Q_H_L_63k3NbwKxUwyqIqv4cgHje5HEt8Ei-_foO-gVc6UFa-QVYnkuIPPDl4XV3ob-3ZOa-JBKfUXJ7G2MpRmO_YJFeUCLV5IRvJXrIVBM92X8qNHa7_KER1sgaKr3JE4wtL5RTfanbCWNNbAK-2fHTfhhNQ")
+MUSIC_UIN = os.getenv("MUSIC_UIN", 635344130)
+headers = {
+    "Host": "u6.y.qq.com",
+    "Cookie": f"qm_keyst={MUSIC_KEY}; uin=o0{str(MUSIC_UIN)}",
+    "content-type": "application/x-www-form-urlencoded",
+    "accept": "application/json",
+    "sec-fetch-site": "same-site",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-fetch-mode": "cors",
+    "origin": "https://i2.y.qq.com",
+    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12A365 QQMusic/14.4.0 Mskin/black Mcolor/00cc6cff Bcolor/00000000 skinid[901] NetType/WIFI WebView/WKWebView Released[1] en-US DeviceModel/iPhone16,2 skin_css/skin2_1_901 Pixel/1290 FreeFlow/0 teenMode/0 nft_released/[1] ui_mode/1 model130200/1 FontMode/0]  IAP[0]  H5/1",
+    "referer": "https://i2.y.qq.com/",
+    "sec-fetch-dest": "empty",
+}
+
+def get_task():
+    data = {"comm":{"g_tk":929272714,"uin":MUSIC_UIN,"format":"json","inCharset":"utf-8","outCharset":"utf-8","notice":0,"platform":"h5","needNewCode":1,"ct":23,"cv":0,"mesh_devops":"DevopsBase"},"req_0":{"module":"music.actCenter.ActCenterSignNewSvr","method":"GetSignInSummary","param":{"ActID":"Z25hHGi"}},"req_1":{"module":"music.actCenter.ActCenterSignNewSvr","method":"GetSignInTaskList","param":{"ActID":"Z25hHGi","ScenesID":"3"}},"req_2":{"module":"music.actCenter.ActCenterSignNewSvr","method":"GetSignInCalendar","param":{"ActID":"Z25hHGi"}}}
+    data = json.dumps(data, separators=(',', ':'))
+    url = f"https://u6.y.qq.com/cgi-bin/musics.fcg?_webcgikey=GetSignInSummary_GetSignInTaskList_GetSignInCalendar&_={current_millis_str()}&sign={zzc_sign(data)}"
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    response_json = response.json()
+    daily_tasks = response_json["req_1"]["data"]["TaskListInfo"]["TaskList"]["ContinueTaskList"]
+    pending_task_ids = []
+    for idx, task in daily_tasks.items():
+        task_id = task["ID"]
+        task_name = task["Name"]
+        task_status = task["State"]
+        if task_status != 1:
+            pending_task_ids.append(task_id)
+        print(f"Task {str(idx)}: {task_name} (ID: {task_id}, Status: {str(task_status)})")
+    return pending_task_ids
+
+def sign_in(task_id):
+    data = {"comm":{"g_tk":929272714,"uin":MUSIC_UIN,"format":"json","inCharset":"utf-8","outCharset":"utf-8","notice":0,"platform":"h5","needNewCode":1,"ct":23,"cv":0,"mesh_devops":"DevopsAccSignIn"},"req_0":{"module":"music.actCenter.ActCenterSignNewSvr","method":"AwardPrize","param":{"ActID":"Z25hHGi","TaskID":f"{task_id}"}}}
+    data = json.dumps(data, separators=(',', ':'))
+    url = f"https://u6.y.qq.com/cgi-bin/musics.fcg?_webcgikey=AwardPrize&_={current_millis_str}&sign={zzc_sign(data)}"
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    response_json = response.json()
+    print(f"Response：{response_json}")
+    send_bark_notification(title='QQMusic', body=f'每日签到Response。\n{response_json}', group_name='QQMusic')
+
+pending_tasks = get_task()
+for task_id in pending_tasks:
+    sign_in(task_id)
