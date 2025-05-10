dbc52d00cbec6fe1ac2098fac445156e591e9fdc
Chris <vipfwz@gmail.com>
2025-05-10 04:05:08 +0000

    actual signin

--- qq_music/sign_in.py
+++ qq_music/sign_in.py
@@ -35,16 +35,22 @@ def get_task():
     response_json = response.json()
     daily_tasks = response_json["req_1"]["data"]["TaskListInfo"]["TaskList"]["ContinueTaskList"]
     pending_task_ids = []
+    today_task_id = ""
     for idx, task in daily_tasks.items():
         task_id = task["ID"]
         task_name = task["Name"]
         task_status = task["State"]
         if task_status != 1:
             pending_task_ids.append(task_id)
+        if task_status == 2:
+            today_task_id = task_id
         print(f"Task {str(idx)}: {task_name} (ID: {task_id}, Status: {str(task_status)})")
-    return pending_task_ids
+    return today_task_id
 
-def sign_in(task_id):
+def award_price(task_id):
+    if not task_id:
+        print("No pending award found for today.")
+        return
     data = {"comm":{"g_tk":929272714,"uin":MUSIC_UIN,"format":"json","inCharset":"utf-8","outCharset":"utf-8","notice":0,"platform":"h5","needNewCode":1,"ct":23,"cv":0,"mesh_devops":"DevopsAccSignIn"},"req_0":{"module":"music.actCenter.ActCenterSignNewSvr","method":"AwardPrize","param":{"ActID":"Z25hHGi","TaskID":f"{task_id}"}}}
     data = json.dumps(data, separators=(',', ':'))
     url = f"https://u6.y.qq.com/cgi-bin/musics.fcg?_webcgikey=AwardPrize&_={current_millis_str}&sign={zzc_sign(data)}"
@@ -52,8 +58,18 @@ def sign_in(task_id):
     response.raise_for_status()
     response_json = response.json()
     print(f"Response：{response_json}")
-    send_bark_notification(title='QQMusic Daily Signin', body=f'{task_id}: {response_json["req_0"]["data"]["retMsg"]}', group_name='QQMusic')
 
-pending_tasks = get_task()
-for task_id in pending_tasks:
-    sign_in(task_id)
+def sign_in():
+    data = {"comm":{"g_tk":929272714,"uin":MUSIC_UIN,"format":"json","inCharset":"utf-8","outCharset":"utf-8","notice":0,"platform":"h5","needNewCode":1,"ct":23,"cv":0,"mesh_devops":"DevopsBase"},"req_0":{"module":"music.actCenter.ActCenterSignNewSvr","method":"SignIn","param":{"ActID":"Z25hHGi","ScenesID":"3"}}}
+    data = json.dumps(data, separators=(',', ':'))
+    url = f"https://u6.y.qq.com/cgi-bin/musics.fcg?_webcgikey=SignIn&_={current_millis_str}&sign={zzc_sign(data)}"
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    response_json = response.json()
+    print(f"Response：{response_json}")
+    if response_json.get("req_0", {}).get("data", {}).get("Info").get("IsSignIn") == 1:
+        send_bark_notification(title='QQMusic Daily Signin', body=f'Successfully Signin! Monly Signin Count:{response_json["req_0"]["data"]["Info"]["MonthSignInCount"]} Days', group_name='QQMusic')
+
+sign_in()
+today_task_id = get_task()
+award_price(today_task_id)
